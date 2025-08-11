01429f31f065748b547c99c7756e5fb46a3301c2
Julian981309 <julianarroyo263510@gmail.com>
2025-08-11 09:00:11 -0500

    Update createProducto.cy.js

--- cypress/e2e/demo/createProducto.cy.js
+++ cypress/e2e/demo/createProducto.cy.js
@@ -1,16 +1,1 @@
-import LoginPage from '../../support/pages/LoginPage'
-import ProductosPage from '../../support/pages/ProductosPage'
 
-describe('Crear Producto', () => {
-  beforeEach(() => {
-    LoginPage.visit();
-    LoginPage.fillLogin('usuario@demo.com', 'password');
-    LoginPage.submit();
-    ProductosPage.visit();
-  });
-
-  it('Debe crear un producto nuevo', () => {
-    ProductosPage.crearProducto('Camiseta', '19.99');
-    ProductosPage.verificarProducto('Camiseta');
-  });
-});
